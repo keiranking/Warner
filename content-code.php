775c53a434c73b5a877b7038fc49351b4ba366d6
keiranking <me@keiranking.com>
2018-01-18 23:05:41 -0800

    Tags for code posts.

--- content-code.php
+++ content-code.php
@@ -16,7 +16,7 @@
   </h5>
 
   <h2 class="title"><?php the_title(); ?></h2>
-
+  <div class="tags"><?php the_tags('', ' &bullet; '); ?></div>
   <div class="content">
     <?php the_content(); ?>
     <?php
